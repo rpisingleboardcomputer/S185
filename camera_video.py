d5a63e1ec73937da1857a097f328689afbc0f272
RPi Single-Board Computer <{ID}+{username}@users.noreply.github.com>
2018-10-31 19:32:32 -0500

    Update to the EOL format.

--- camera_video.py
+++ camera_video.py
@@ -4,12 +4,16 @@
 """
 S185 - Introduction to Single-Board Computers
 
-A simple python script to record video to a file using the 
-Raspberry Pi Camera Module.
+A python script to record video to a file using the Raspberry Pi Camera Module.
 
 Execution:
 1. Set the executable flag on the script: chmod u+x camera_video.py
 2. Execute with a preceding dot "./", so call ./camera_video.py
+
+To play play the video, enter the following command:
+omxplayer video.h264
+or
+omxplayer video.mjpeg
 """
 
 #  Import required modules.
@@ -29,17 +33,26 @@ camera = picamera.PiCamera()
 
 # Define the main() function.
 def main():
+	# Set the camera resolution.
+	camera.resolution = (640, 480)
+
+	# Set the camera framerate.
+	camera.framerate = 20
+
 	# Start recording video from the camera, storing it in output.
 	#	Write an H.264 video stream.
-	#camera.start_recording('/home/pi/Videos/video.h264')
+	#camera.start_recording('/home/pi/Videos/video.h264', format='h264')
 	# Write an M-JPEG video stream.
-	camera.start_recording('/home/pi/Videos/video.mjpeg')
+	camera.start_recording('/home/pi/Videos/video.mjpeg', format='mjpeg')
 
-	# Pause,capture 10 seconds of video.
-	time.sleep(10)
+	# Capture 10 seconds of video.
+	camera.wait_recording(10)
 
 	# Stop recording video from the camera.
 	camera.stop_recording()
+
+	# Stop the video preview.
+	camera.stop_preview()
 # EOF: main()
 
 
@@ -61,4 +74,4 @@ if __name__ == '__main__':
 		# This method stops all recording and preview activities and releases
 		#	all resources associated with the camera; this is necessary to
 		#	prevent GPU memory leaks.
-      camera.close()
+		camera.close()
