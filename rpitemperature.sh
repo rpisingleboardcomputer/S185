f1e82eeb0f837a449197e976729638f182423d56
RPi Single-Board Computer <{ID}+{username}@users.noreply.github.com>
2019-07-07 12:21:45 -0500

    Formatting and commented lines deletion.

--- rpitemperature.sh
+++ rpitemperature.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-# -------------------------------------------------------------
-# Script to display the CPU & GPU temperature of a Raspberry Pi
-# in the console.
-# NOTE: (1)	To make the shell script executable:
-#		chmod u+x rpitemperature.sh
-#	(2)	Uses a command line calculator, bc.
-#		sudo apt-get install bc
-# -------------------------------------------------------------
-# Get the temperature of the CPU, in Celsius
+# -------------------------------------------------------------------
+# Shell script to display the CPU & GPU temperature of a Raspberry Pi
+# in a terminal/console.
+# NOTE: (*) Uses the command line calculator, bc, for the temperature
+#           conversion.
+#           $ sudo apt-get install bc
+# To make the shell script executable:
+#           $ chmod u+x rpitemperature.sh
+# -------------------------------------------------------------------
+# Get the temperature of the CPU, in Celsius.
 cpuTemp0=$(cat /sys/class/thermal/thermal_zone0/temp)
 cpuTemp1=$(($cpuTemp0/1000))
 cpuTemp2=$(($cpuTemp0/100))
 cpuTempM=$(($cpuTemp2 % $cpuTemp1))
-#  Convert the CPU temperature to Fahrenheit
+#  Convert the CPU temperature from Celsius to Fahrenheit.
 cpuTempF1=$[(${cpuTemp1}*9/5)+32]
 cpuTempF2=$[(${cpuTemp2}*9/5)+32]
 cpuTempF=$(( $cpuTempF2 % $cpuTempF1 ))
 
-# Get the temperature of the GPU, in Celsius
-#gpuTemp0=$(/opt/vc/bin/vcgencmd measure_temp)
-#gpuTemp0=${gpuTemp0//\'/º}
-#gpuTemp0=${gpuTemp0//temp=/}
+# Get the temperature of the GPU, in Celsius.
 gpuTemp0=$(/opt/vc/bin/vcgencmd measure_temp|cut -c6-9)
 gpuTempF=$(echo "scale=2;((9/5) * $gpuTemp0) + 32" | bc)
 
-# Display the CPU and GPU temperature to the console
+# Display the CPU and GPU temperatures, Celsius and Fahrenheit, to the
+#  terminal/console.
 echo CPU Temp: $cpuTemp1"."$cpuTempM"ºC" / $cpuTempF1"."$cpuTempF"ºF"
 echo GPU Temp: $gpuTemp0"ºC" / $gpuTempF"ºF"
